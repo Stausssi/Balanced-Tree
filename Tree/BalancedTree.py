f8c6aa42351eb6143d48c17a8b9d3dac0616b6ab
Niklas Drössler <n-droessler@outlook.de>
2021-10-27 11:33:53 +0200

    Added search

--- Tree/BalancedTree.py
+++ Tree/BalancedTree.py
@@ -23,12 +23,13 @@ class BalancedTree:
 
     def search(self, key):
         """
-        Searches the balanced tree for a given key
+        Searches the balanced tree for a given key from the root
 
         Args:
             key(int): Key that is searched for in the balanced tree
 
         Returns:
+            int: An Integer if the key was found, else None
 
         """
         # recursively search the tree for "key"
@@ -39,56 +40,24 @@ class BalancedTree:
         Recursively searches for the key in the given node
 
         Args:
-            node:
-            key_to_search:
+            node(Node): The Node that should be searched recursively
+            key_to_search(int): Key
 
         Returns:
+            Tuple(Node,int): An Integer if the key was found, else None
 
         """
 
-        # todo: edge case: Key List not full    [1,   2,   None,None]
-        # todo:                       nodes:[R1,   R2,  R3, None, None
-        # if key is none, search R3 (is None ? --> kann nicht sein, weil immer n+1 sons)
-
-        # iterate over node.keys, until key_to_search is no longer smaller than the key
-        for key in node.keys:
-            if key == key_to_search:
-                # found key
-                return key
-            elif key_to_search < key:
-                # The index of "key" is equal to index of the child node that should be searched next.
-                # This is seen in the example below for key_to_search = 2, where (key_to_search < key)
-                # is first true for key=3 (node.keys[1]). Since the keys with values less than 3 and
-                # greater than 2 lie by definition in the subtree referenced by R2, one should recursively
-                # search this son. R2(node.sons[1]) has the same index as the current key.
-
-                # node.keys:            [1,   3,   7,   8]
-                # correspondences:      /    /    /    /
-                # node.sons:         [R1,  R2,  R3,  R4,  R5]
-
-                child_index = node.keys.index(key)
-
-                # recursively search the child node and return it´s key
-                return self.__recursive_search(node.keys[child_index], key_to_search)
+        if node.hasKey(key_to_search):
+            # the key is returned, data could also be returned
+            return self, key_to_search
         else:
-            # This get´s executed, then the for-loop completes, this only happens, if "key_to_search" is greater
-            # than all of the nodes keys.
-            # This can happen in the following cases:
-            # 1) The key is in the subtree, where the last reference of node.sons points to.
-            #    If "key_to_search" is 9 for example, this would indicate,
-            #    that the key is in R5 since 9 is greater than 8.
-            #         node.keys:      [1,   3,   7,   8]
-            # correspondences:        /    /    /    /
-            #         node.sons:   [R1,  R2,  R3,  R4,  R5]
-            # 2) The reference to the last son is "None", which means that "key_to_search" is not in the tree
-
-            last_node = node.sons[::-1]
-            if last_node is None:
-                # returning None indicates, no key was found in the tree
-                return None
+            # determine child node to search recursively
+            child_node = node.getSubtree(key_to_search)
+            if child_node is None:
+                return None, None
             else:
-                # recursively search the last node
-                return self.__recursive_search(last_node, key_to_search)
+                return self.__recursive_search(child_node, key_to_search)
 
 
 class Node:
@@ -103,21 +72,66 @@ class Node:
     def __init__(self, k, isRoot=False):
         self.k = k
         self.isRoot = isRoot
-        self.keys = [None] * (2 * k)  # every node with k sons should save max k-1 keys
-        self.sons = [None] * (2 * k + 1)  # max k sons, references child nodes
+        self.keys = [None] * (2 * k)  # min k max 2k entries
+        self.sons = [None] * (2 * k + 1)  # max 2k + 1 sons, references child nodes
 
-    def addKeys(self):
+    def hasKey(self, key):
         """
+        Checks if a node contains a key
+
+        Args:
+            key(int): The key that should be checked
 
         Returns:
+            bool: True, if key is in node
 
         """
-        pass
+
+        return key in self.keys
+
+    def getSubtree(self, key_to_search):
+        """
+
+        Args:
+            key_to_search(int): Key
+
+        Returns:
+            Node: child node, whose subtree contains the key_to_search
+
+        """
+
+        for index, key in enumerate(self.keys):
+            if key is None or key_to_search < key:
+                # The index of "key" is equal to index of the child node that should be searched next.
+                # This is seen in the example below for key_to_search = 2, where (key_to_search < key)
+                # is first true for key=3 (node.keys[1]). Since the keys with values less than 3 and
+                # greater than 2 lie by definition in the subtree referenced by R2, one should recursively
+                # search this son. R2(node.sons[1]) has the same index as the current key.
+                # node.keys:            [1,   3,   7,   8]
+                # correspondences:      /    /    /    /
+                # node.sons:         [R1,  R2,  R3,  R4,  R5]
+
+                # When not all keys are occupied, and key_to_search is greater than all keys, the loop needs to stop
+                # at the first None occurrence and pick the child node at that index as seen below
+                # node.keys:            [1,   3,  None, None]
+                # correspondences:      /    /    /    /
+                # node.sons:         [R1,  R2,  R3,  R4,  R5] --> R3 would need to be picked
+
+                # return child node, can be None
+                return self.sons[index]
+
+        # This get´s executed, when the for-loop completes, this only happens, if "key_to_search" is greater
+        # than all of the nodes keys. The key is in the subtree, where the last reference of node.sons points to.
+
+        return self.sons[-1]
 
     def isLeaf(self):
         """
+        Checks, if the node is a leaf (has no children)
 
         Returns:
+            bool: True if node is a leaf
 
         """
+
         return self.sons == [None] * (2 * self.k + 1)
