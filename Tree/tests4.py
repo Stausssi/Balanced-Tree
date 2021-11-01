d1df810acd79b5bd04d04eda314785cd6c36e2fe
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-11-01 16:05:24 +0100

    modified test

--- Tree/tests4.py
+++ Tree/tests4.py
@@ -2,13 +2,9 @@ from Tree.BalancedTree import BalancedTree
 
 bt = BalancedTree(1)
 
-keys = [1, 22, 21, 12, 3, 11, 7, 123]
+keys = [1, 22, 21, 12, 3, 11, 7, 123, 88]
 
 for key in keys:
     bt.insert(key)
     print(f" ------------- Insert {key} ----------------")
     print(bt)
-
-
-bt.insert(88)
-print(bt)
