d39687e26c2e3d026f466723c8619383c42be83e
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-11-03 10:52:08 +0100

    bugtests

--- Tree/tests/bug1.py
+++ Tree/tests/bug1.py
@@ -4,19 +4,19 @@ import random
 bt = BalancedTree(2)
 
 insert = [x for x in range(30)]
-random.Random(4).shuffle(insert)
+# random.Random(4).shuffle(insert)
 
 delete = [19, 22, 21, 27, 18, 26, 25, 17, 16, 8]
 
 for key in insert:
-    print(f" ------------- Insert {key} ----------------")
+    #print(f" ------------- Insert {key} ----------------")
     bt.insert(key)
-    print(bt, "\n")
+    #print(bt, "\n")
 
 for key in delete:
-    print(f"-------------- Delete {key} -----------------")
+    #print(f"-------------- Delete {key} -----------------")
     bt.delete(key)
-    print(bt, "\n")
+    #print(bt, "\n")
 
 print("hi")
 
